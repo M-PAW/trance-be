a22f65b6673607c7e109909d70a1ddced9611b0d
Michael Walter <michael_patrick.walter@outlook.com>
2021-09-08 22:10:02 -0700

    Built out auth router and endpoints, login, refresh token. Not yet fully tested.

--- Routes/Lyrics.js
+++ Routes/Lyrics.js
@@ -4,6 +4,6 @@ const findLyrics = require('./Lyrics-Helpers/findLyrics')
 LyricsRouter.get('/', (req,res) => {
     const {artist, track} = req.body;
     findLyrics({artist,track}, res)
-})
+});
 
-module.exports = LyricsRouter
+module.exports = LyricsRouter;
--- index.js
+++ index.js
@@ -3,21 +3,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const LyricsRouter = require('./Routes/Lyrics')
+const LyricsRouter = require('./Routes/Lyrics');
+const AuthRouter = require('./Routes/Auth');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/lyrics/', LyricsRouter)
+app.use('/lyrics', LyricsRouter);
+app.use('/auth', AuthRouter);
 
 let PORT = process.env.PORT || 5050;
-
-
-app.get('/', (req,res) => {
-    res.status(200).send('Server Online!')
-});
 
 app.listen(PORT, () => {
     console.log(`Server Online: ${PORT}`);
